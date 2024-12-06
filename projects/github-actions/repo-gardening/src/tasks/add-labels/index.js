1f3eb80f7638f07b45ba0b513b253a8414d7210c
tbradsha <32492176+tbradsha@users.noreply.github.com>
2024-12-06 12:31:05 -0700

    Update index.js

--- projects/github-actions/repo-gardening/src/tasks/add-labels/index.js
+++ projects/github-actions/repo-gardening/src/tasks/add-labels/index.js
@@ -337,7 +337,7 @@ async function addLabels( payload, octokit ) {
 	// If the PR title includes the word "revert", mark it as such.
 	const isRevert = title.toLowerCase().includes( 'revert' );
 
-	const currentLabels = await getLabels( octokit, owner, repo, number );
+	const currentLabels = await getLabels( octokit, owner, repository, number );
 
 	const labelsToAdd = await getLabelsToAdd( octokit, owner.login, name, number, isDraft, isRevert );
 
